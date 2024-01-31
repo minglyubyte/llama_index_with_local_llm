d67ad7442277f20e98fd9d1a869bc147db10a222
Ming Lyu <leo@Mings-MacBook-Pro.local>
2024-01-30 17:20:01 -0800

    finetune

---
+++ utils.py
@@ -0,0 +1,27 @@
+import os
+import torch
+import pandas as pd
+import numpy as np
+from datasets import load_dataset, Dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    TrainingArguments,
+    pipeline,
+    logging
+)
+from peft import LoraConfig, PeftModel, PeftConfig
+from trl import SFTTrainer
+import time
+from dotenv import load_dotenv
+import random
+
+def print_number_of_trainable_model_parameters(model):
+    trainable_model_params = 0
+    all_model_params = 0
+    for _, param in model.named_parameters():
+        all_model_params += param.numel()
+        if param.requires_grad:
+            trainable_model_params += param.numel()
+    return f"trainable model parameters: {trainable_model_params}\nall model parameters: {all_model_params}\npercentage of trainable model parameters: {100 * trainable_model_params / all_model_params:.2f}%"
